a904b293e53c30eb30bcfbe06031c8e8865c862a
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2017-03-07 15:15:47 -0400

    Avance de Glory Days

---
+++ GloryDays/MemoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryCell.swift
+//  GloryDays
+//
+//  Created by Eduardo De La Cruz on 7/3/17.
+//  Copyright © 2017 Eduardo De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell
+{
+    
+}
--- GloryDays/ViewController.swift
+++ GloryDays/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func askForPermissions(_ sender: UIButton)
+    {
+        
+    }
 }
-
